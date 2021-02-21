de31479650b507bbd4d0e3a0e5a6928a124ade08
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2021-02-21 21:56:06 +0100

    Did some more tests on local storage, so far so good

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -1,1 +1,26 @@
-document.write(localStorage.getItem("myHouse"))
+if (localStorage.getItem("myHouse") == null){
+    document.write("The db is empty")
+    db = {
+        "kitchen": {
+            "drawer":["glass", "plate", "plate"],
+            "fridge":["yogurt", "fish", "meat"]
+        },
+        "living room":{
+            "snack drawer":["ketchup chips", "strawberry milka", "penut mnms"]
+        }
+
+    }
+    document.write(JSON.stringify(db))
+    localStorage.setItem("myHouse", JSON.stringify(db))
+    document.write("Db initialized.")
+}else{
+    document.write("db loaded")
+    db = localStorage.getItem("myHouse")
+    parsd = JSON.parse(db)
+    for (x in parsd){
+        document.write(x)
+        for(y in parsd[x]){
+            document.write(y)
+        }
+    }
+}
