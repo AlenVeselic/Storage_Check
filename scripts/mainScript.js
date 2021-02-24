805aad9f8fc5fa956cf7d9f25cb9d8057e10a5cb
Alen Veselič <68716124+AlenVeselic@users.noreply.github.com>
2021-02-24 12:06:52 +0100

    Added two more data input functions, prompts

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -22,6 +22,8 @@ function displayDatabase(){
         headText = document.createTextNode(places[place])
         headerEl.appendChild(headText)
         document.body.appendChild(headerEl)
+        
+        if(localStorage.getItem(places[place]) == "") continue
 
         data = JSON.parse(localStorage.getItem(places[place]))
         keysOne = Object.keys(data)
@@ -61,5 +63,62 @@ function addPlace(){
     if(localStorage.getItem(nameValue) == null){
         localStorage.setItem(nameValue, "")
     }
+
+}
+
+function addRoom(){
+    place = prompt("Which place are you adding this room to?")
+    roomName = document.getElementById("field").value
+
+    allPlaces = Object.keys(localStorage)
+
+    if(allPlaces.includes(place)){
+        if(localStorage.getItem(place) != ""){
+        placeData = JSON.parse(localStorage.getItem(place))
+        }else{
+           placeData = {}
+        }
+        placeData[roomName] = ""
+        localStorage.setItem(place, JSON.stringify(placeData))
+    }else{
+
+        window.alert("The place " + place + " doesn't exist")
+
+    }
+}
+
+function addFurniture(){
+    
+    allPlaces = Object.keys(localStorage)
+
+    place = prompt("Which place would you like to add this piece of furniture?")
+
+    if(allPlaces.includes(place)){
+        if(localStorage.getItem(place) != ""){
+        placeData = JSON.parse(localStorage.getItem(place)) 
+        }else{
+            placeData = {}
+        }
+
+    }
+    if(Object.keys(placeData) != 0){
+        allRooms = Object.keys(placeData)
+    }else{
+        window.alert("This place doesn't have any rooms.")
+    }
+
+    room = prompt("Which room in " + place + " would you like to add this piece of furniture?")
+    if(allRooms.includes(room)){
+    furnitureName = document.getElementById("field").value
+    placeData[room] = {}
+    placeData[room][furnitureName] = ""
+
+    localStorage.setItem(place, JSON.stringify(placeData))
+    window.alert("Furniture successfuly added.")
+
+    }else{
+        window.alert("That room doesn't exist.")
+    }
+
 
 }
